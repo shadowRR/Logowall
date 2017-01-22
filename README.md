6c4a458eb2d4bdad0acedeb7d99a79863a4131f7
Shad <shadowrr2@gmail.com>
2017-01-22 11:36:57 +0100

    now fully working

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Wallpaper from code / text
 
 ### Build a printable source code poster with your code / text and your logo.
-( Interface is a work-in-progress, but you can already build your poster )
 
 Ex:
 ![alt text](https://raw.githubusercontent.com/shadowRR/Logowall/master/img/examples/tux_logo_code_wallpaper_small.png "Code Wallpaper with Tux Logo")
-![alt text](https://raw.githubusercontent.com/shadowRR/Logowall/master/img/examples/ubuntu_logo_wallpaper_small.png "Code Wallpaper with Ubuntu Logo")
-![alt text](https://raw.githubusercontent.com/shadowRR/Logowall/master/img/examples/gimp_logo_wallpaper_small.png "Code Wallpaper with Gimp Logo")
--- scripts/build.js
+++ scripts/build.js
@@ -5,60 +5,24 @@
  * @license MIT
  */
 
+let TEXT;
+let LOGO;
+let CANVAS;
+let CANVASCONTEXT;
+let CANVASIMG;
+
 $( document ).ready( () => {
 
 	// get the base canvas
-	const canvas = document.getElementById( 'canvas' );
+	CANVAS = document.getElementById( 'canvas' );
+	CANVASCONTEXT = CANVAS.getContext( '2d' );
 	// and the canvas who will hold the image used for the text colors
-	const canvasImg = document.getElementById( 'canvas-img' );
+	CANVASIMG = document.getElementById( 'canvas-img' );
 
-	fitCanvasToContainer( canvas );
-	fitCanvasToContainer( canvasImg );
+	fitCanvasToContainer( CANVAS );
+	fitCanvasToContainer( CANVASIMG );
 
-	// get some minified code to draw on the canvas
-	$.get( 'https://code.jquery.com/jquery-3.1.1.min.js', function( data ) {
-
-		const ctx = canvas.getContext( '2d' );
-		ctx.font = '7px Arial';
-
-		const width = canvas.width;
-		const height = canvas.height;
-
-		// handle the special canvas with img for color
-		const ctxImg = canvasImg.getContext( '2d' );
-		var image = document.getElementById( 'img' );
-		ctxImg.drawImage( image,
-        	canvas.width / 2 - image.width / 2,
-        	canvas.height / 2 - image.height / 2 );
-		// ctxImg.contrast = 50;
-
-		let x = 6;
-		let y = 8;
-		for( let i = 0; i < data.length; i++ ) {
-
-			// get the color at the text position
-			const img_data = ctxImg.getImageData( x, y, 6, 8 ).data;
-			let color = "#" + ( "000000" + rgbToHex( img_data[ 0 ], img_data[ 1 ], img_data[ 2 ] ) ).slice( -6 );
-
-			if( color === '#ffffff' || color === '#000000' ) {
-				color = '#a5a5a5';
-			}
-
-			ctx.fillStyle = color;
-			ctx.fillText( data[ i ], x, y );
-			x = x + 6;
-
-			if( x > width ) {
-				x = 6;
-				y = y + 8;
-
-				if( y > height ) {
-					break;
-				}
-			}
-		}
-
-	} );
+	$( '#generate' ).on( 'click', buildPoster );
 
 } );
 
@@ -72,6 +36,105 @@ function fitCanvasToContainer( canvas ){
 	canvas.style.height='100%';
 	canvas.width  = canvas.offsetWidth;
 	canvas.height = canvas.offsetHeight;
+}
+
+/**
+ * get the text from the given url
+ * @param  {String} url
+ * @author shad
+ */
+function getText( callback ) {
+	const text_url = $( '#text_url' ).val();
+
+	if( !text_url ) {
+		throw 'no url for the text was given';
+	}
+
+	$.get( text_url, function( data ) {
+		TEXT = data.toString();
+		callback();
+	} );
+}
+
+/**
+ * get the logo url
+ * @author shad
+ */
+function getLogo( callback ) {
+	const logo = $( '#logo' );
+	const logo_img = logo[ 0 ].files[ 0 ];
+
+	if( !logo_img ) {
+		throw 'no file for the logo was given';
+	}
+
+	LOGO = logo_img;
+	callback();
+}
+
+/**
+ * build the poster
+ * @author shad
+ */
+function buildPoster() {
+
+	getLogo( function() {
+		getText( function() {
+
+			CANVASCONTEXT.font = '7px Arial';
+
+			const width = CANVAS.width;
+			const height = CANVAS.height;
+
+			// handle the special CANVAS with img for color
+			const ctxImg = CANVASIMG.getContext( '2d' );
+			const logo = document.getElementById( 'img' );
+
+			const reader = new FileReader();
+			reader.addEventListener( 'load', function() {
+			    logo.src = reader.result;
+			}, false );
+
+			$( logo ).on( 'load', function() {
+
+				ctxImg.drawImage( img,
+					CANVAS.width / 2 - img.width / 2,
+					CANVAS.height / 2 - img.height / 2 );
+				// ctxImg.contrast = 50;
+
+				let x = 6;
+				let y = 8;
+				for( let i = 0; i < TEXT.length; i++ ) {
+
+					// get the color at the text position
+					const img_data = ctxImg.getImageData( x, y, 6, 8 ).data;
+					let color = "#" + ( "000000" + rgbToHex( img_data[ 0 ], img_data[ 1 ], img_data[ 2 ] ) ).slice( -6 );
+
+					if( color === '#ffffff' || color === '#000000' ) {
+						color = '#a5a5a5';
+					}
+
+					CANVASCONTEXT.fillStyle = color;
+					CANVASCONTEXT.fillText( TEXT[ i ], x, y );
+					x = x + 6;
+
+					if( x > width ) {
+						x = 6;
+						y = y + 8;
+
+						if( y > height ) {
+							break;
+						}
+					}
+				}
+
+			} );
+
+			reader.readAsDataURL( LOGO );
+
+		} );
+	} );
+
 }
 
 /**
